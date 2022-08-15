387adcc9b5662c83ec7a30854ed17e6492ace245
Marco Valino <marco200002010@outlook.com>
2022-08-15 16:19:26 -0300

    Update index.js

--- src/componentes/EspacoJogos/index.js
+++ src/componentes/EspacoJogos/index.js
@@ -319,121 +319,146 @@ const EspacoJogos = (props) => {
             imagemMeio: './imagens/grazinha_bkg.png',
         }
     ]
+    debugger
+    return (
 
-    if (props.aparecerJogo === jogos[0].titulo) {
-        return (
-            <section className='espaco-jogos'>
-                <div>
-                        <Jogo
-                            key={jogos[0].titulo}
-                            titulo={jogos[0].titulo}
-                            texto1={jogos[0].texto1}
-                            texto2={jogos[0].texto2}
-                            imagem1={jogos[0].imagem1}
-                            imagem2={jogos[0].imagem2}
-                            imagemMeio={jogos[0].imagemMeio}
-                        />
-                </div>
-    
-            </section>
-        )
-    }
+        jogos.map(jogo =>
+            
+            props.aparecerJogo === jogo.titulo && <section className='espaco-jogos'>
+            <div>
+                {
+                    <Jogo
+                        key={jogo.titulo}
+                        titulo={jogo.titulo}
+                        texto1={jogo.texto1}
+                        texto2={jogo.texto2}
+                        imagem1={jogo.imagem1}
+                        imagem2={jogo.imagem2}
+                        imagemMeio={jogo.imagemMeio}
+                    />
+                }
+                
+            </div>
+        </section>
+    )
+    )
 
-    else if (props.aparecerJogo === jogos[1].titulo) {
-        return (
-            <section className='espaco-jogos'>
-                <div>
-                        <Jogo
-                            key={jogos[1].titulo}
-                            titulo={jogos[1].titulo}
-                            texto1={jogos[1].texto1}
-                            texto2={jogos[1].texto2}
-                            imagem1={jogos[1].imagem1}
-                            imagem2={jogos[1].imagem2}
-                            imagemMeio={jogos[1].imagemMeio}
-                        />
-                </div>
-    
-            </section>
-        )
-    }
 
-    else if (props.aparecerJogo === jogos[2].titulo) {
-        return (
-            <section className='espaco-jogos'>
-                <div>
-                        <Jogo
-                            key={jogos[2].titulo}
-                            titulo={jogos[2].titulo}
-                            texto1={jogos[2].texto1}
-                            texto2={jogos[2].texto2}
-                            imagem1={jogos[2].imagem1}
-                            imagem2={jogos[2].imagem2}
-                            imagemMeio={jogos[2].imagemMeio}
-                        />
-                </div>
+    /*
+        if (props.aparecerJogo === jogos[0].titulo) {
+            return (
+                <section className='espaco-jogos'>
+                    <div>
+                            <Jogo
+                                key={jogos[0].titulo}
+                                titulo={jogos[0].titulo}
+                                texto1={jogos[0].texto1}
+                                texto2={jogos[0].texto2}
+                                imagem1={jogos[0].imagem1}
+                                imagem2={jogos[0].imagem2}
+                                imagemMeio={jogos[0].imagemMeio}
+                            />
+                    </div>
+                </section>
+            )
+        }
     
-            </section>
-        )
-    }
+        else if (props.aparecerJogo === jogos[1].titulo) {
+            return (
+                <section className='espaco-jogos'>
+                    <div>
+                            <Jogo
+                                key={jogos[1].titulo}
+                                titulo={jogos[1].titulo}
+                                texto1={jogos[1].texto1}
+                                texto2={jogos[1].texto2}
+                                imagem1={jogos[1].imagem1}
+                                imagem2={jogos[1].imagem2}
+                                imagemMeio={jogos[1].imagemMeio}
+                            />
+                    </div>
+        
+                </section>
+            )
+        }
+    
+        else if (props.aparecerJogo === jogos[2].titulo) {
+            return (
+                <section className='espaco-jogos'>
+                    <div>
+                            <Jogo
+                                key={jogos[2].titulo}
+                                titulo={jogos[2].titulo}
+                                texto1={jogos[2].texto1}
+                                texto2={jogos[2].texto2}
+                                imagem1={jogos[2].imagem1}
+                                imagem2={jogos[2].imagem2}
+                                imagemMeio={jogos[2].imagemMeio}
+                            />
+                    </div>
+        
+                </section>
+            )
+        }
+    
+        else if (props.aparecerJogo === jogos[3].titulo) {
+            return (
+                <section className='espaco-jogos'>
+                    <div>
+                            <Jogo
+                                key={jogos[3].titulo}
+                                titulo={jogos[3].titulo}
+                                texto1={jogos[3].texto1}
+                                texto2={jogos[3].texto2}
+                                imagem1={jogos[3].imagem1}
+                                imagem2={jogos[3].imagem2}
+                                imagemMeio={jogos[3].imagemMeio}
+                            />
+                    </div>
+        
+                </section>
+            )
+        }
+    
+        else if (props.aparecerJogo === jogos[4].titulo) {
+            return (
+                <section className='espaco-jogos'>
+                    <div>
+                            <Jogo
+                                key={jogos[4].titulo}
+                                titulo={jogos[4].titulo}
+                                texto1={jogos[4].texto1}
+                                texto2={jogos[4].texto2}
+                                imagem1={jogos[4].imagem1}
+                                imagem2={jogos[4].imagem2}
+                                imagemMeio={jogos[4].imagemMeio}
+                            />
+                    </div>
+        
+                </section>
+            )
+        }
+    
+        else if (props.aparecerJogo === jogos[5].titulo) {
+            return (
+                <section className='espaco-jogos'>
+                    <div>
+                            <Jogo
+                                key={jogos[5].titulo}
+                                titulo={jogos[5].titulo}
+                                texto1={jogos[5].texto1}
+                                texto2={jogos[5].texto2}
+                                imagem1={jogos[5].imagem1}
+                                imagem2={jogos[5].imagem2}
+                                imagemMeio={jogos[5].imagemMeio}
+                            />
+                    </div>
+        
+                </section>
+            )
+        }
+        */
 
-    else if (props.aparecerJogo === jogos[3].titulo) {
-        return (
-            <section className='espaco-jogos'>
-                <div>
-                        <Jogo
-                            key={jogos[3].titulo}
-                            titulo={jogos[3].titulo}
-                            texto1={jogos[3].texto1}
-                            texto2={jogos[3].texto2}
-                            imagem1={jogos[3].imagem1}
-                            imagem2={jogos[3].imagem2}
-                            imagemMeio={jogos[3].imagemMeio}
-                        />
-                </div>
-    
-            </section>
-        )
-    }
-
-    else if (props.aparecerJogo === jogos[4].titulo) {
-        return (
-            <section className='espaco-jogos'>
-                <div>
-                        <Jogo
-                            key={jogos[4].titulo}
-                            titulo={jogos[4].titulo}
-                            texto1={jogos[4].texto1}
-                            texto2={jogos[4].texto2}
-                            imagem1={jogos[4].imagem1}
-                            imagem2={jogos[4].imagem2}
-                            imagemMeio={jogos[4].imagemMeio}
-                        />
-                </div>
-    
-            </section>
-        )
-    }
-
-    else if (props.aparecerJogo === jogos[5].titulo) {
-        return (
-            <section className='espaco-jogos'>
-                <div>
-                        <Jogo
-                            key={jogos[5].titulo}
-                            titulo={jogos[5].titulo}
-                            texto1={jogos[5].texto1}
-                            texto2={jogos[5].texto2}
-                            imagem1={jogos[5].imagem1}
-                            imagem2={jogos[5].imagem2}
-                            imagemMeio={jogos[5].imagemMeio}
-                        />
-                </div>
-    
-            </section>
-        )
-    }
-    
 }
 
 export default EspacoJogos
